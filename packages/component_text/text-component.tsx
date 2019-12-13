bddbf2acbf58c5cc8434af7747479905c9cca3c5
Artem Mirochnik <amironich1993@gmail.com>
2019-12-13 15:28:50 +0300

    add copm

--- packages/component_text/text-component.tsx
+++ packages/component_text/text-component.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import {generateText} from 'generate_text';
 
 export const TextComponent = ({text}) => <div>
-    {text}
+    {text}l
     {generateText()}
     {1+1}
 </div>;
--- packages/generate_text/generate_text.js
+++ packages/generate_text/generate_text.js
@@ -1,5 +1,5 @@
 import {uniqueId} from 'lodash/uniqueId';
 
 export const generateText = () => {
-    return uniqueId() + 'unnqueffff';
+    return uniqueId();
 };
